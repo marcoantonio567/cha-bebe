40d369a797c4682a851016edacdb20c14bff2766
marcoantonio567 <2mbetwin@gmail.com>
2024-03-27 13:48:58 -0300

    alterando a static_files e o index.css

--- cha_bebe/settings.py
+++ cha_bebe/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "app/static"),
+    os.path.join(BASE_DIR, "app/static/"),
 )
 
 # Default primary key field type
